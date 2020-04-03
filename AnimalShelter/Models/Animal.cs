dd4ba599504d54e3ab4bba8542a78d369b8e4bad
Geoff Goetz <geoff.goetz503@gmail.com>
2020-04-03 14:23:12 -0700

    Add  validation

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -1,11 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AnimalShelter.Models
 {
     public class Animal
     {
         public int AnimalId { get; set; }
+        
+        [Required]
+        [StringLength(30)]
         public string Name { get; set; }
+        
+        [Required]
         public string Species { get; set; }
+
+        [Required]
+        [Range(0, 40, ErrorMessage = "Cats & dogs don't live longer than 40!.")]
         public int Age { get; set; }
+        [Required]
         public string Gender { get; set; }
     }
 }
