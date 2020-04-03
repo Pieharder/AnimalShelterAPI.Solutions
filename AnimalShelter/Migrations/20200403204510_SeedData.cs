e571344d129eb7eb3e8a37a9c1f974eaa23d079b
Geoff Goetz <geoff.goetz503@gmail.com>
2020-04-03 13:54:38 -0700

    Add seed data migration

---
+++ AnimalShelter/Migrations/20200403204510_SeedData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Gender", "Name", "Species" },
+                values: new object[,]
+                {
+                    { 1, 7, "Female", "Tardar Sauce", "Ragdoll Cat" },
+                    { 2, 3, "Female", "Huginn", "Bombay Cat" },
+                    { 3, 3, "Male", "Muninn", "Bombay Cat" },
+                    { 4, 4, "Male", "Odin", "Labrador Retriever" },
+                    { 5, 8, "Female", "Boncuk", "Tabby Cat" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5);
+        }
+    }
+}
