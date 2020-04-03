5d41293602a39b06a7e56a17dce390c5217a6fd3
Geoff Goetz <geoff.goetz503@gmail.com>
2020-04-03 13:55:07 -0700

    Add db seed data.

--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -2,13 +2,26 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AnimalShelter.Models
 {
-    public class AnimalShelterContext : DbContext
+  public class AnimalShelterContext : DbContext
+  {
+    public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
+        : base(options)
     {
-        public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
-            : base(options)
-        {
-        }
 
-        public DbSet<Animal> Animals { get; set; }
     }
+
+    public DbSet<Animal> Animals { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Animal>()
+				.HasData(
+					new Animal { AnimalId = 1, Name = "Tardar Sauce", Species = "Ragdoll Cat", Age = 7, Gender = "Female" },
+					new Animal { AnimalId = 2, Name = "Huginn", Species = "Bombay Cat", Age = 3, Gender = "Female" },
+					new Animal { AnimalId = 3, Name = "Muninn", Species = "Bombay Cat", Age = 3, Gender = "Male" },
+					new Animal { AnimalId = 4, Name = "Odin", Species = "Labrador Retriever", Age = 4, Gender = "Male" },
+					new Animal { AnimalId = 5, Name = "Boncuk", Species = "Tabby Cat", Age = 8, Gender = "Female" }
+				);
+    }
+  }
 }
